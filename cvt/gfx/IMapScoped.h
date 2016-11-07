5ee911eb9f2651a63f544f5e7741e0913f8390e9
Philipp Heise <philipp.heise@bmw-carit.de>
2016-11-07 17:18:53 +0100

    Add bilinear and channel access to IMapScoped
    	modified: gfx/IMapScoped.h
    
    Change-Id: Ic8ce2b3ba369a35a79bed051282d3835ce88ef72

--- cvt/gfx/IMapScoped.h
+++ cvt/gfx/IMapScoped.h
@@ -2,6 +2,7 @@
    The MIT License (MIT)
 
    Copyright (c) 2011 - 2013, Philipp Heise and Sebastian Klose
+   Copyright (c) 2016, BMW Car IT GmbH, Philipp Heise (philipp.heise@bmw.de)
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +22,6 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
 */
-
 #ifndef CVT_IMAPSCOPED_H
 #define CVT_IMAPSCOPED_H
 
@@ -30,161 +30,207 @@
 
 namespace cvt {
 
-	template<typename T>
-		class IMapScoped {
-			private:
-				template<typename T2>
-					struct ITypeSelector;
+    template<typename T>
+    class IMapScoped {
+        private:
+            template<typename T2>
+            struct ITypeSelector;
 
-				template<typename T2>
-					struct ITypeSelector
-					{
-						typedef Image& IType;
-						typedef uint8_t* IPtrType;
-					};
+            template<typename T2>
+            struct ITypeSelector
+            {
+                typedef Image& IType;
+                typedef uint8_t* IPtrType;
+            };
 
-				template<typename T2>
-					struct ITypeSelector<const T2>
-					{
-						typedef const Image& IType;
-						typedef const uint8_t* IPtrType;
-					};
+            template<typename T2>
+            struct ITypeSelector<const T2>
+            {
+                typedef const Image& IType;
+                typedef const uint8_t* IPtrType;
+            };
 
-				typedef typename ITypeSelector<T>::IType IType;
-				typedef typename ITypeSelector<T>::IPtrType IPtrType;
+            typedef typename ITypeSelector<T>::IType IType;
+            typedef typename ITypeSelector<T>::IPtrType IPtrType;
 
-			public:
-				IMapScoped( IType img );
-				~IMapScoped();
+        public:
+            IMapScoped( IType img );
+            ~IMapScoped();
 
-				void		reset();
-				T*			ptr();
-				T*			ptr() const;
-				T*			base();
-				T*			line( size_t y );
-				void		prevLine();
-				void		nextLine();
-				void		setLine( size_t l );
-				void		operator++( int );
-				void		operator--( int );
-				size_t		stride() const;
-				T&			operator()( size_t x, size_t y );
-				const T&	operator()( size_t x, size_t y ) const;
-				size_t		width() const;
-				size_t		height() const;
+            void        reset();
+            T*          ptr();
+            T*          ptr() const;
+            T*          base();
+            T*          line( size_t y );
+            void        prevLine();
+            void        nextLine();
+            void        setLine( size_t l );
+            void        operator++( int );
+            void        operator--( int );
+            size_t      stride() const;
+            T&          operator()( size_t x, size_t y );
+            const T&    operator()( size_t x, size_t y ) const;
+            T&          operator()( size_t x, size_t y, size_t channel );
+            const T&    operator()( size_t x, size_t y, size_t channel ) const;
+            size_t      width() const;
+            size_t      height() const;
+            const T     bilinear( float x, float y ) const;
+            const T     bilinear( float x, float y, size_t channel ) const;
 
-			private:
-				IType	    _img;
-				IPtrType	_base;
-				IPtrType	_line;
-				size_t		_stride;
-		};
+        private:
+            IType       _img;
+            IPtrType    _base;
+            IPtrType    _line;
+            size_t      _stride;
+            size_t      _channels;
+    };
 
-	template<typename T>
-	inline IMapScoped<T>::IMapScoped( IMapScoped<T>::IType img ) : _img( img )
-	{
-		_base = _img.map( &_stride );
-		_line = _base;
-	}
+    template<typename T>
+    inline IMapScoped<T>::IMapScoped( IMapScoped<T>::IType img ) : _img( img )
+    {
+        _base = _img.map( &_stride );
+        _line = _base;
+        _channels = _img.channels();
+    }
 
-	template<typename T>
-	inline IMapScoped<T>::~IMapScoped( )
-	{
-		_img.unmap( _base );
-	}
+    template<typename T>
+    inline IMapScoped<T>::~IMapScoped( )
+    {
+        _img.unmap( _base );
+    }
 
-	template<typename T>
-	inline void IMapScoped<T>::reset()
-	{
-		_line = _base;
-	}
+    template<typename T>
+    inline void IMapScoped<T>::reset()
+    {
+        _line = _base;
+    }
 
-	template<typename T>
-	inline T* IMapScoped<T>::ptr()
-	{
-		return ( T* ) _line;
-	}
+    template<typename T>
+    inline T* IMapScoped<T>::ptr()
+    {
+        return ( T* ) _line;
+    }
 
-	template<typename T>
-	inline T* IMapScoped<T>::ptr() const
-	{
-		return ( T* ) _line;
-	}
+    template<typename T>
+    inline T* IMapScoped<T>::ptr() const
+    {
+        return ( T* ) _line;
+    }
 
-	template<typename T>
-	inline T* IMapScoped<T>::base()
-	{
-		return ( T* ) _base;
-	}
+    template<typename T>
+    inline T* IMapScoped<T>::base()
+    {
+        return ( T* ) _base;
+    }
 
 
-	template<typename T>
-	inline T* IMapScoped<T>::line( size_t y )
-	{
-		return ( T* ) ( _base + _stride * y );
-	}
+    template<typename T>
+    inline T* IMapScoped<T>::line( size_t y )
+    {
+        return ( T* ) ( _base + _stride * y );
+    }
 
-	template <typename T>
-	inline void IMapScoped<T>::setLine( size_t l )
-	{
-		if( l >= _img.height() )
-			throw CVTException( "Trying to set line higher than actual image height!" );
-		_line = _base + _stride * l;
-	}
+    template <typename T>
+    inline void IMapScoped<T>::setLine( size_t l )
+    {
+        if( l >= _img.height() )
+            throw CVTException( "Trying to set scanline to higher value than actual image height!" );
+        _line = _base + _stride * l;
+    }
 
-	template<typename T>
-	inline void IMapScoped<T>::nextLine()
-	{
-		_line += _stride;
-	}
+    template<typename T>
+    inline void IMapScoped<T>::nextLine()
+    {
+        _line += _stride;
+    }
 
-	template<typename T>
-	inline void IMapScoped<T>::prevLine()
-	{
-		_line -= _stride;
-	}
+    template<typename T>
+    inline void IMapScoped<T>::prevLine()
+    {
+        _line -= _stride;
+    }
 
-	template<typename T>
-	inline void IMapScoped<T>::operator++( int )
-	{
-		_line += _stride;
-	}
+    template<typename T>
+    inline void IMapScoped<T>::operator++( int )
+    {
+        _line += _stride;
+    }
 
-	template<typename T>
-	inline void IMapScoped<T>::operator--( int )
-	{
-		_line -= _stride;
-	}
+    template<typename T>
+    inline void IMapScoped<T>::operator--( int )
+    {
+        _line -= _stride;
+    }
 
-	template<typename T>
-	inline size_t IMapScoped<T>::stride() const
-	{
-		return _stride;
-	}
+    template<typename T>
+    inline size_t IMapScoped<T>::stride() const
+    {
+        return _stride;
+    }
 
-	template<typename T>
-	inline T& IMapScoped<T>::operator()( size_t x, size_t y )
-	{
-		return *( ( T* ) ( _base + y * _stride + x * sizeof( T ) ) );
-	}
+    template<typename T>
+    inline T& IMapScoped<T>::operator()( size_t x, size_t y )
+    {
+        return *( ( T* ) ( _base + y * _stride + x * sizeof( T ) ) );
+    }
 
-	template<typename T>
-	inline const T& IMapScoped<T>::operator()( size_t x, size_t y ) const
-	{
-		return *( ( const T* ) ( _base + y * _stride + x * sizeof( T ) ) );
-	}
+    template<typename T>
+    inline const T& IMapScoped<T>::operator()( size_t x, size_t y ) const
+    {
+        return *( ( const T* ) ( _base + y * _stride + x * sizeof( T ) ) );
+    }
 
-	template<typename T>
-	inline size_t IMapScoped<T>::width() const
-	{
-		return _img.width();
-	}
+    template<typename T>
+    inline T& IMapScoped<T>::operator()( size_t x, size_t y, size_t channel )
+    {
+        return *( ( T* ) ( _base + y * _stride + ( x * _channels + channel ) * sizeof( T ) ) );
+    }
 
-	template<typename T>
-	inline size_t IMapScoped<T>::height() const
-	{
-		return _img.height();
-	}
+    template<typename T>
+    inline const T& IMapScoped<T>::operator()( size_t x, size_t y, size_t channel ) const
+    {
+        return *( ( const T* ) ( _base + y * _stride + ( x * _channels + channel ) * sizeof( T ) ) );
+    }
+
+    template<typename T>
+    inline const T IMapScoped<T>::bilinear( float x, float y ) const
+    {
+        int lx = Math::floor( x );
+        int ly = Math::floor( y );
+
+        float alpha1 = x - ( float ) lx;
+        float alpha2 = y - ( float ) ly;
+
+        T v1 = Math::mix( operator()( lx, ly ), operator() ( lx + 1, ly ), alpha1 );
+        T v2 = Math::mix( operator()( lx, ly + 1 ), operator() ( lx + 1, ly + 1 ), alpha1 );
+        return Math::mix( v1, v2, alpha2 );
+    }
+
+    template<typename T>
+    inline const T IMapScoped<T>::bilinear( float x, float y, size_t c ) const
+    {
+        int lx = Math::floor( x );
+        int ly = Math::floor( y );
+
+        float alpha1 = x - ( float ) lx;
+        float alpha2 = y - ( float ) ly;
+
+        T v1 = Math::mix( operator()( lx, ly, c ), operator() ( lx + 1, ly, c ), alpha1 );
+        T v2 = Math::mix( operator()( lx, ly + 1, c ), operator() ( lx + 1, ly + 1, c ), alpha1 );
+        return Math::mix( v1, v2, alpha2 );
+    }
+
+    template<typename T>
+    inline size_t IMapScoped<T>::width() const
+    {
+        return _img.width();
+    }
+
+    template<typename T>
+    inline size_t IMapScoped<T>::height() const
+    {
+        return _img.height();
+    }
 
 }
 
