32db3e9b8a2a7afcbd7ec4738156dd612721c50d
Philipp Heise <Philipp.Heise@bmw.de>
2018-03-27 17:02:23 +0200

    ImageAllocatorCL copy reuses allocated memory
    	modified:   ../cvt/gfx/ImageAllocatorCL.cpp
    
    Change-Id: I6c230203da812247d45e6860524ed0f1f6d9e94d

--- cvt/gfx/ImageAllocatorCL.cpp
+++ cvt/gfx/ImageAllocatorCL.cpp
@@ -2,6 +2,7 @@
    The MIT License (MIT)
 
    Copyright (c) 2011 - 2013, Philipp Heise and Sebastian Klose
+   Copyright (c) 2018, BMW Car IT GmbH, Philipp Heise (philipp.heise@bmw.de)
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -23,87 +24,91 @@
 */
 
 #include <cvt/gfx/ImageAllocatorCL.h>
+#include <cvt/cl/CLCommandQueue.h>
 #include <cvt/util/SIMD.h>
 
 namespace cvt {
 
-	ImageAllocatorCL::ImageAllocatorCL() : ImageAllocator(), _climage( NULL )
-	{
-	}
+    ImageAllocatorCL::ImageAllocatorCL() : ImageAllocator(), _climage( NULL )
+    {
+    }
 
-	ImageAllocatorCL::~ImageAllocatorCL()
-	{
-		if( _climage )
-			delete _climage;
-	}
+    ImageAllocatorCL::~ImageAllocatorCL()
+    {
+        if( _climage )
+            delete _climage;
+    }
 
-	void ImageAllocatorCL::alloc( size_t width, size_t height, const IFormat & format )
-	{
-		if( _width == width && _height == height && _format == format && _climage )
-			return;
+    void ImageAllocatorCL::alloc( size_t width, size_t height, const IFormat & format )
+    {
+        if( _width == width && _height == height && _format == format && _climage )
+            return;
 
-		if( _climage )
-			delete _climage;
-		_width = width;
-		_height = height;
-		_format = format;
+        if( _climage )
+            delete _climage;
+        _width = width;
+        _height = height;
+        _format = format;
 
-		CLImageFormat clformat;
-		format.toCLImageFormat( clformat );
-		_climage = new CLImage2D( _width, _height, clformat, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR  );
-	}
+        CLImageFormat clformat;
+        format.toCLImageFormat( clformat );
+        _climage = new CLImage2D( _width, _height, clformat, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR  );
+    }
 
-	void ImageAllocatorCL::copy( const ImageAllocator* x, const Recti* r = NULL )
-	{
-		if( x->type() != IALLOCATOR_CL ) {
-			const uint8_t* src;
-			const uint8_t* osrc;
-			size_t sstride, dstride;
-			uint8_t* dst;
-			uint8_t* odst;
-			size_t i, n;
-			Recti rect( 0, 0, ( int ) x->_width, ( int ) x->_height );
-			SIMD* simd = SIMD::instance();
+    void ImageAllocatorCL::copy( const ImageAllocator* x, const Recti* r = NULL )
+    {
+        if( x->type() != IALLOCATOR_CL ) {
+            const uint8_t* src;
+            const uint8_t* osrc;
+            size_t sstride, dstride;
+            uint8_t* dst;
+            uint8_t* odst;
+            size_t i, n;
+            Recti rect( 0, 0, ( int ) x->_width, ( int ) x->_height );
+            SIMD* simd = SIMD::instance();
 
-			if( r )
-				rect.intersect( *r );
-			alloc( rect.width, rect.height, x->_format );
+            if( r )
+                rect.intersect( *r );
+            alloc( rect.width, rect.height, x->_format );
 
-			osrc = src = x->map( &sstride );
-			src += rect.y * sstride + x->_format.bpp * rect.x;
-			odst = dst = map( &dstride );
-			n =  x->_format.bpp * rect.width;
+            osrc = src = x->map( &sstride );
+            src += rect.y * sstride + x->_format.bpp * rect.x;
+            odst = dst = map( &dstride );
+            n =  x->_format.bpp * rect.width;
 
-			i = rect.height;
-			while( i-- ) {
-				simd->Memcpy( dst, src, n );
-				dst += dstride;
-				src += sstride;
-			}
-			x->unmap( osrc );
-			unmap( odst );
-		} else {
-			const ImageAllocatorCL* clmem = ( ImageAllocatorCL* ) x;
-			// FIXME: try to reuse _climage if possible
-			if( _climage )
-				delete _climage;
-			_climage = new CLImage2D( *( clmem->_climage ), r, CL_MEM_READ_WRITE | CL_MEM_ALLOC_HOST_PTR  );
-		}
-	}
+            i = rect.height;
+            while( i-- ) {
+                simd->Memcpy( dst, src, n );
+                dst += dstride;
+                src += sstride;
+            }
+            x->unmap( osrc );
+            unmap( odst );
+        } else {
+            Recti rect( 0, 0, ( int ) x->_width, ( int ) x->_height );
+            if( r )
+                rect.intersect( *r );
 
-	uint8_t* ImageAllocatorCL::map( size_t* stride )
-	{
-		return ( uint8_t* ) _climage->map( stride );
-	}
+            alloc( rect.width, rect.height, x->_format );
 
-	const uint8_t* ImageAllocatorCL::map( size_t* stride ) const
-	{
-		return ( const uint8_t* ) _climage->map( stride );
-	}
+            const ImageAllocatorCL* clmem = ( ImageAllocatorCL* ) x;
+            CL::defaultQueue()->enqueueCopyImage( *_climage, *( clmem->_climage ), 0, 0, rect.x, rect.y, rect.width, rect.height, NULL, NULL );
+        }
+    }
 
-	void ImageAllocatorCL::unmap( const uint8_t* ptr ) const
-	{
-		_climage->unmap( ( const void* ) ptr );
-	}
+    uint8_t* ImageAllocatorCL::map( size_t* stride )
+    {
+        return ( uint8_t* ) _climage->map( stride );
+    }
+
+    const uint8_t* ImageAllocatorCL::map( size_t* stride ) const
+    {
+        return ( const uint8_t* ) _climage->map( stride );
+    }
+
+    void ImageAllocatorCL::unmap( const uint8_t* ptr ) const
+    {
+        _climage->unmap( ( const void* ) ptr );
+    }
 
 }
